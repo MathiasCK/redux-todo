ac35425abfdae21f321b3c7d754525b11d3ea11d
Mathias Kolberg <mathias.ck2000@gmail.com>
2021-11-18 12:47:31 +0100

    Styles

--- src/components/TodoAdder/Form-Input.js
+++ src/components/TodoAdder/Form-Input.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
+import './styles/todoadder-styles.css';
 
 const FormInput = ({ handleChange, label, ...otherProps }) => (
-  <Group>
-    <StyledFormInput onChange={handleChange} {...otherProps} />
+  <section className='group'>
+    <input className='input' onChange={handleChange} {...otherProps} />
     {label ? (
       <label
         className={`${
@@ -13,46 +13,7 @@ const FormInput = ({ handleChange, label, ...otherProps }) => (
         {label}
       </label>
     ) : null}
-  </Group>
+  </section>
 );
-
-const Group = styled.div`
-  position: relative;
-  .form-input-label {
-    color: grey;
-    font-size: 16px;
-    font-weight: normal;
-    position: absolute;
-    pointer-events: none;
-    left: 5px;
-    top: 10px;
-    transition: 300ms ease all;
-    &.shrink {
-      top: -14px;
-      font-size: 12px;
-      color: black;
-    }
-  }
-`;
-
-const StyledFormInput = styled.input`
-  background: #f7f7f7;
-  color: grey;
-  font-size: 18px;
-  padding: 10px 10px 10px 5px;
-  display: block;
-  width: 100%;
-  border: none;
-  border-radius: 0;
-  border-bottom: 1px solid grey;
-  &:focus {
-    outline: none;
-  }
-  &:focus ~ .form-input-label {
-    top: -14px;
-    font-size: 12px;
-    color: black;
-  }
-`;
 
 export default FormInput;
